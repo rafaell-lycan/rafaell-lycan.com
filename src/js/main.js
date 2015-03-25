74e75b4c1ef3ad851c4695bc4763eafcd4480e13
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-03-25 01:56:06 -0300

    Update about links

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,18 @@
   Document    : main.js
   Author      : Rafaell Lycan
 */
-if (document.location.hostname !== 'localhost') {
-  /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-  var disqus_shortname = 'rafaelllycan'; // required: replace example with your forum shortname
+(function () {
+  'use strict';
+  if (document.location.hostname !== 'localhost') {
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqusShortname = 'rafaelllycan'; // required: replace example with your forum shortname
 
-  /* * * DON'T EDIT BELOW THIS LINE * * */
-  (function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-  })();
-}
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+      var dsq = document.createElement('script');
+      dsq.type  = 'text/javascript'; dsq.async = true;
+      dsq.src = '//' + disqusShortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+  }
+})();
