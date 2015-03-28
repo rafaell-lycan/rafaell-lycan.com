b9d5aae856ac69fa0be9d9616946b6a82687bbe9
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-03-28 03:37:51 -0300

    Update About info and make some modifications on CSS
    - Improve Gruntfile with new plugins and describes
    - Change RSS file
    - Done LESS and Grunt posts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,100 +1,84 @@
-module.exports = function (grunt){
+module.exports = function (grunt) {
+  'use strict';
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-jekyll');
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-   // Project configuration
-   grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-      concat: { // Concat JS files
-         js: {
-            options: {
-               separator: ';'
-            },
-            src: 'src/js/main.js',
-            dest: 'assets/js/main.min.js'
-         },
-      }, // end Concat
-
-      uglify: { // Minify JS files
-         options: {
-           mangle: false
-         },
-         js: {
-           files: {
-             'assets/js/main.min.js': ['assets/js/main.min.js']
-           }
-         },
-       }, // end Uglify
-
-      less: { // Compile LESS files
-        default: {
-          options: {
-            compress: true,
-          },
-          files: { // Dest : Origin
-            "assets/css/style.css": ['src/less/style.less']
-          },
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          'assets/css/style.css' : 'src/less/style.less'
         }
-      }, // end less
-
-      watch: { // Watch JS and LESS folder
-        js: {
-          files: ['src/js/*.js'],
-          tasks: ['concat:js', 'uglify:js','jekyll'],
-          options: {
-            livereload: true,
-          }
-        },
-        less: {
-            files: ['src/less/**/*.less'],
-            tasks: ['less','jekyll'],
-            options: {
-              livereload: true,
-            },
-         },
-         html: {
-            files: ['./**/*.html','./**/*.md','!./node_modules/*.html'],
-            tasks: ['jekyll'],
-            options: {
-              livereload: true,
-            },
-         },
-         markdown: {
-            files: ['_drafts/*.md','_posts/*.md'],
-            tasks: ['jekyll'],
-            options: {
-              livereload: true,
-            },
-         },
-      }, // end watch
-
-      jekyll: {
-        build: {
-          options: {
-            drafts: true
-          },
-        },
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false
       },
-
-      connect: { // Start a server in localhost:4000
-        server: {
-          options: {
-            port: 4000,
-            hostname: '*',
-            base: './_publish',
-            livereload: true
-          }
+      js: {
+        files: {
+          'assets/js/main.min.js': ['assets/js/main.min.js']
         }
-      } // end connect
+      },
+    },
+    jekyll: {
+      options: {
+        drafts: true
+      },
+      dist: {
+        options: {
+          dest: './_publish',
+          config: '_config.yml'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      css: {
+        files: ['src/less/**/*.less'],
+        tasks: ['less', 'jekyll']
+      },
+      js: {
+        files: ['src/js/**/.js'],
+        tasks: ['uglify', 'jekyll']
+      },
+      html: {
+        files: ['**/*.html', '!./node_modules/*.html'],
+        tasks: ['jekyll'],
+        options: {
+          spawn: false,
+        }
+      },
+      markdown: {
+        files: ['_drafts/*.md', '_posts/*.md'],
+        tasks: ['jekyll'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 4000,
+          base: './_publish',
+        }
+      }
+    },
 
-   });
+  });
 
-   grunt.registerTask('build', ['concat', 'uglify', 'less', 'jekyll', 'connect', 'watch']);
-   grunt.registerTask('default', ['build']);
+  grunt.registerTask('dev', ['less', 'uglify', 'jekyll', 'connect', 'watch']);
+  grunt.registerTask('build', ['less', 'uglify', 'jekyll']);
+  grunt.registerTask('default', ['dev']);
 };
