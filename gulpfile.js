20b5f0c64be786babfee8533b5b71a5bb85f9a05
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-11-03 11:34:18 -0200

    Change build system from Grunt to Gulp + Browsersync

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync'),
+    cp = require('child_process');
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+  return cp.spawn('jekyll', ['build', '-D'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+  browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['less', 'scripts', 'jekyll-build'], function() {
+  browserSync({
+    server: {
+      baseDir: '_publish'
+    },
+    host: 'localhost',
+    open: false
+  });
+});
+
+/**
+ * Compile files from assets/less into both _publish/css (for live injecting) and assets (for future jekyll builds)
+ */
+gulp.task('less', function () {
+    return gulp.src('src/less/style.less')
+        .pipe(less())
+        .pipe(autoprefixer(['last 2 version', 'ie 9'], { cascade: true }))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('_publish/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/css'));
+});
+
+/**
+ * Compile files from assets/js into both _publish/js (for live injecting) and assets (for future jekyll builds)
+ */
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('_publish/assets/js'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('assets/js'));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('src/js/**/*.js', ['scripts']);
+    gulp.watch('src/less/**/*.less', ['less']);
+    gulp.watch(['*.{html,md}', '_layouts/*.{html,md}', '_posts/*', '_drafts/*', '!assets', '!_publish'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
