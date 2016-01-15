0424d0c3417e9984f23c433ed92f3bfb867242f2
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-01-15 16:56:17 -0200

    Update Disqus script

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,7 @@
   Document    : main.js
   Author      : Rafaell Lycan
 */
-(function () {
+;(function () {
   'use strict';
-  if (document.location.hostname !== 'localhost') {
-    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    var disqusShortname = 'rafaelllycan'; // required: replace example with your forum shortname
-
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function () {
-      var dsq = document.createElement('script');
-      dsq.type  = 'text/javascript'; dsq.async = true;
-      dsq.src = '//' + disqusShortname + '.disqus.com/embed.js';
-      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
-  }
+  // Nothing here!
 })();
