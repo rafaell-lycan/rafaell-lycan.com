c373ac9ad14b6ca88d989453a361a24db0762740
Rafaell Lycan <sonny.webdsg@gmail.com>
2017-04-25 03:58:00 +0200

    Revert "Changed to 2.0 layout."

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-console.log('Hello!')
+$(function() {
+  $('.load').animate({opacity: "1"},1000);
+  $('.load-right').each(function(i) {
+    $(this).delay((i++) * 200).animate({left:0, opacity:"1"});
+  });
+});
