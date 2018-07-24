1ed7268d3b409e8d165bcf7acc2277882cee9fef
Rafaell Lycan <sonny.webdsg@gmail.com>
2018-07-24 17:30:00 +0200

    Change Service Work implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
 const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const SWPrecache = require('sw-precache-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const path = require('path');
-
-const rootDir = "_site";
 
 module.exports = {
   entry: {
@@ -45,45 +42,6 @@ module.exports = {
       output: {
         comments: false,
       },
-    }),
-    new SWPrecache({
-      cacheId: `pwa-v1-${Date.now()}`,
-      filename: 'sw.js',
-      staticFileGlobs: [
-        `${rootDir}/index.html`,
-        `${rootDir}/manifest.json`,
-        `${rootDir}/404.html`,
-        `${rootDir}/assets/scripts/main.js`,
-        `${rootDir}/assets/images/bg-elements.png`,
-        `${rootDir}/assets/images/me.jpg`,
-        `${rootDir}/assets/{styles,scripts,icons,fonts,svgs}/*`,
-        `${rootDir}/{articles,about,offline}/*.html`,
-        // `${rootDir}/**/*.html`,
-      ],
-      stripPrefix: `${rootDir}`,
-      minify: true,
-      verbose: true,
-      mergeStaticsConfig: true,
-      navigateFallback: `/offline`,
-      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/, /\.html$/],
-      runtimeCaching: [
-        {
-          handler: 'networkFirst',
-          urlPattern: /^https?.*/
-        },
-        {
-          handler: 'cacheFirst',
-          urlPattern: /^https:\/\/fonts\.googleapis\.com\//
-        },
-        {
-          handler: 'cacheFirst',
-          urlPattern: /^https:\/\/ajax\.googleapis\.com\//
-        },
-        {
-          handler: 'cacheFirst',
-          urlPattern: /^https:\/\/google-analytics\.com\//
-        }
-      ]
     }),
     new CompressionPlugin({
       asset: '[path].gz[query]',
